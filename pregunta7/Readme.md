77663ebe730e00a99d34e9c36fdb9edb3ffac6fc
Amaranta <villegasamaranta@gmail.com>
2023-12-13 03:06:13 -0400

    Reto Velocista

---
+++ pregunta7/Readme.md
@@ -0,0 +1,21 @@
+# EXAMEN 3 - CI3641 - PREGUNTA 7
+# AMARANTA VILLEGAS 16-11247
+
+## Instrucciones de Ejecucion
+
+1. Abra la terminal o linea de comandos de linux (o alguna de sus distribuciones) o windows.
+2. Navegue al directorio donde se encuentra el codigo fuente.
+3. Ejecute el programa con el siguiente comando :
+
+    Si su sistema operativo es una distribucion de Linux
+
+    ``` python3 Velocista.py ```
+
+    Si su sistema operativo es Windows
+
+    ``` py Velocista.py ```
+
+4. Requisitos del Sistema
+
+- Se requiere tener instalado una version de python superior a la 3.8.10
+- Sistema operativo: Linux o Windows (puede funcionar en otros sistemas, pero se ha probado en estos).
---
+++ pregunta7/Velocista.py
@@ -0,0 +1,33 @@
+"""
+CI3641 - LENGUAJES DE PROGRAMACION I
+EXAMEN3 - PREGUNTA 4
+AMARANTA VILLEGAS 16-11247
+Velocista.py : programa que permita calcular valores para wadefoc(n) de la forma más
+eficiente posible, para valores lo más grandes posibles de n.
+"""
+import time
+from math import log2
+
+def wadefoc(n):
+    actual = 0
+    sig = 1
+    for i in range(n + 1):
+        temp = actual
+        actual = sig
+        sig = sig + temp
+    return actual
+
+# Entrada del usuario
+n = int(input("n: "))
+
+inicio = time.time()
+# Calcular logaritmo en base 2 y agregar 1 para redondear hacia arriba
+log_valor = log2(wadefoc(n + 1)) + 1
+
+final = time.time()
+# Imprimir el resultado
+print(wadefoc(int(log_valor)))
+
+tiempo = round(final - inicio, 6)
+
+print("\ntiempo:" , tiempo)
